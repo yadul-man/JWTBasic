c9f2e47a1c6d2e47151a5a4f4b7cbdfae61a66fd
yadul <yadulmanoj22@gmail.com>
2024-10-20 18:44:38 +0530

    Added JWT Authorization to WeatherForecast controller

--- JWTBasic/Controllers/WeatherForecastController.cs
+++ JWTBasic/Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWTBasic.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
